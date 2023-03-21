4511ca7c55b3d682dd7320ab5738ef8946acab89
Yousef Na <72698881+yousef-RXy@users.noreply.github.com>
2023-03-22 01:29:46 +0200

    set up the base movement

--- Source/shooter/ShooterCharacter.cpp
+++ Source/shooter/ShooterCharacter.cpp
@@ -7,13 +7,15 @@
 
 
 // Sets default values
-AShooterCharacter::AShooterCharacter()
+AShooterCharacter::AShooterCharacter():
+	BaseTurnRate(45.f),
+	BaseLookUpRate(45.f)
 {
 	PrimaryActorTick.bCanEverTick = true;
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom-> SetupAttachment(RootComponent);
-	CameraBoom-> TargetArmLength=300.f;//the camer follows at this distance behind the character
+	CameraBoom-> TargetArmLength=300.f;//the camera follows at this distance behind the character
 	CameraBoom-> bUsePawnControlRotation =true;//rotate the arm based on the controller
 	//create the follow camera
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
@@ -30,6 +32,43 @@ void AShooterCharacter::BeginPlay()
 	UE_LOG(LogTemp, Warning, TEXT("Some warning message!"));
 }
 
+void AShooterCharacter::MoveForward(float Value)
+{
+	if ((Controller != nullptr)&& (Value!=0.0f))
+	{
+		//find which way is forward
+		const FRotator Rotation{ Controller->GetControlRotation() };
+		const FRotator YawRotation{ 0, Rotation.Yaw, 0 };
+
+		const FVector Direction{FRotationMatrix{YawRotation}.GetUnitAxis(EAxis::X)};
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AShooterCharacter::MoveRight(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f))
+	{
+		//find which way is right
+		const FRotator Rotation{ Controller->GetControlRotation() };
+		const FRotator YawRotation{ 0, Rotation.Yaw, 0 };
+		const FVector Direction{ FRotationMatrix{YawRotation}.GetUnitAxis(EAxis::Y) };
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AShooterCharacter::TurnAtRate(float Rate)
+{
+	//calculate the delta for this frames from thr rate information
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AShooterCharacter::LookUpAtRate(float Rate)
+{
+	//calculate the delta for this frames from thr rate information
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
 // Called every frame
 void AShooterCharacter::Tick(float DeltaTime)
 {
@@ -41,6 +80,18 @@ void AShooterCharacter::Tick(float DeltaTime)
 void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	check(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AShooterCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AShooterCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("TurnRate", this, &AShooterCharacter::TurnAtRate);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &AShooterCharacter::LookUpAtRate);
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+
+	PlayerInputComponent->BindAction("Jump",IE_Pressed,this,&ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
 
 }
 
--- Source/shooter/ShooterCharacter.h
+++ Source/shooter/ShooterCharacter.h
@@ -18,6 +18,22 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	//called for forwards/backwards input
+	void MoveForward(float Value);
+	//called for side to side input
+	void MoveRight(float Value);
+
+	/*
+	*	called via input to turn at given rate
+	*		@param rate this is a normalized rate, i.e. 1.0 means100% of desired turn rate
+	*/
+	void TurnAtRate(float Rate);
+	
+	/*
+	* called via input to look up/down at given rate
+	* @param rate this is a normalized rate, i.e. 1.0 means100% of desired turn rate
+	*/
+	void LookUpAtRate(float Rate);
 
 public:	
 	// Called every frame
@@ -27,12 +43,21 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 private:
-	/*camera boom positionig the camera behind the character*/
+	/*camera boom positioning the camera behind the character*/
 	UPROPERTY(VisibleAnyWhere, BlueprintReadOnly, Category = camera, meta = (AllowPrivateAccess = "true"))
 	class USpringArmComponent* CameraBoom;
 	/*camera that follow the character*/
 	UPROPERTY(VisibleAnyWhere, BlueprintReadOnly, Category = camera, meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent* FollowCamera;
+
+	//base turn rate and deg/sec. Other scaling may affect final turn rate
+	UPROPERTY(VisibleAnyWhere, BlueprintReadOnly,category=camera, meta = (AllowPrivateAccess = "true"))
+	float BaseTurnRate;
+
+	//base look up/down rate, in deg/sec. Other scaling may affect final turn rate
+	UPROPERTY(VisibleAnyWhere, BlueprintReadOnly, category = camera, meta = (AllowPrivateAccess = "true"))
+	float BaseLookUpRate;
+
 public:
 	/*returns CameraBoom SubObject */
 	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
